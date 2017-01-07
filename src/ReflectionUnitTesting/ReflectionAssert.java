2066e44bafd79657e51e825d0a26cf3dbb6fd9f9
Marcus Newman <marcusgeorgenewman@gmail.com>
2017-01-06 19:51:50 -0800

    ClassExists completed

--- src/ReflectionUnitTesting/ReflectionAssert.java
+++ src/ReflectionUnitTesting/ReflectionAssert.java
@@ -1,7 +1,36 @@
 package ReflectionUnitTesting;
 
+import junit.framework.AssertionFailedError;
+
+/**
+ * Class to test Classes and Methods using reflection.
+ * 
+ * @author Marcus Newman
+ *
+ */
 public class ReflectionAssert {
-	public Class<?> ClassExists(String className) {
-		return null;
+	/**
+	 * Method that tests for the existence of a Class using reflection. If it
+	 * doesn't it throws an {@link AssertionFailedError}.
+	 *
+	 * @param className
+	 *            the name of the class, including package name.
+	 */
+	public void ClassExists(String className) {
+		try {
+			Class.forName(className);
+		} catch (ClassNotFoundException e) {
+			throw new AssertionFailedError();
+		}
+	}
+
+	/**
+	 * Method that tests for the existence of a Method using reflection. If it
+	 * does not it throws an {@link AssertionError}.
+	 * 
+	 * @param methodName
+	 */
+	public void MethodExists(String methodName) {
+
 	}
 }
--- src/ReflectionUnitTestingTests.java
+++ src/ReflectionUnitTestingTests.java
@@ -1,10 +1,16 @@
 import static org.junit.Assert.*;
 import java.lang.reflect.*;
+
+import org.junit.Rule;
 import org.junit.Test;
+import org.junit.rules.ExpectedException;
+
+import junit.framework.AssertionFailedError;
 
 public class ReflectionUnitTestingTests {
 	String className = "ReflectionAssert";
 	String classExistsMethodName = "ClassExists";
+	String methodExistsMethodName = "MethodExists";
 
 	@Test
 	public void ReflectionAssertClassShouldExist() {
@@ -27,21 +33,61 @@ public class ReflectionUnitTestingTests {
 
 	}
 
+	@Test
+	public void MethodExistsMethodShouldExistAndAcceptAString() {
+		Method methodExistsMethod = getMethodExistsMethod();
+		assertNotNull(classExistsMethodName + " method doesn't exist or doesn't accept a string.", methodExistsMethod);
+
+	}
+
 	private Method getClassExistsMethod() {
+		String methodName = classExistsMethodName;
+		return getMethodWithStringParameter(methodName);
+	}
+
+	private Method getMethodExistsMethod() {
+		String methodName = methodExistsMethodName;
+		return getMethodWithStringParameter(methodName);
+	}
+
+	private Method getMethodWithStringParameter(String className) {
+
 		Class<?> reflectionAssertClass = getReflectionAssertClass();
 		Class<?>[] parameterTypes = { String.class };
 		try {
-			return reflectionAssertClass.getMethod(classExistsMethodName, parameterTypes);
+			return reflectionAssertClass.getMethod(className, parameterTypes);
 		} catch (NoSuchMethodException e) {
 			return null;
 		}
 	}
 
-	@Test
-	public void ClassExistsMethodReturnTypeShouldBeClass() {
+	@Rule
+	public ExpectedException thrown = ExpectedException.none();
+
+	@Test(expected = AssertionFailedError.class)
+	public void ClassExistsMethodShouldThrowAnAssertionFailedErrorWithAnInvalidClassName() throws Throwable {
+		thrown.reportMissingExceptionWithMessage(
+				"ClassExists method should throw an assertionFailedError with an invalid class name.");
+		String invalidClassName = "InvalidClassName";
+		Class<?> reflectionAssertClass = getReflectionAssertClass();
 		Method classExistsMethod = getClassExistsMethod();
-		Object result = classExistsMethod.getReturnType();
-		assertTrue("ClassExists method return type should be Class.", result == Class.class);
+		Object[] args = { invalidClassName };
+		try {
+			classExistsMethod.invoke(reflectionAssertClass.newInstance(), args);
+		} catch (InvocationTargetException e) {
+			throw e.getTargetException();
+		}
 	}
 
+	@Test
+	public void ClassExistsMethodShouldNotThrowAnExceptionWithAValidClassName() throws Throwable {
+		Class<?> reflectionAssertClass = getReflectionAssertClass();
+		Method classExistsMethod = getClassExistsMethod();
+		Object[] args = { "ReflectionUnitTesting." + className };
+		try {
+			classExistsMethod.invoke(reflectionAssertClass.newInstance(), args);
+		} catch (InvocationTargetException e) {
+			fail("ClassExists method should not throw an exception with a valid class name.");
+		}
+	}
 }
